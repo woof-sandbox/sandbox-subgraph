cb8a471dbaad596ea179231f86c0fbb033cce86e
Michael <misha.h@woof.software>
2025-07-23 11:45:53 +0200

    fix market accounting updating

--- src/helpers/external/market.ts
+++ src/helpers/external/market.ts
@@ -253,10 +253,11 @@ export function updateMarketAccounting(
   accounting: MarketAccounting,
   event: ethereum.Event
 ): void {
-  if (accounting.lastAccountingUpdatedBlockNumber.equals(event.block.number)) {
+  // !: If left as is, transactions within the same block might not be accounted for.
+  /*if (accounting.lastAccountingUpdatedBlockNumber.equals(event.block.number)) {
     // Don't bother to update if we already did this block, assume this gets set on init
     return;
-  }
+  }*/
 
   const comet = CometContract.bind(Address.fromBytes(market.id));
   const configuration = getOrCreateMarketConfiguration(market, event);
